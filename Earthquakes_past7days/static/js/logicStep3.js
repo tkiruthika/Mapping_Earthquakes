2f8fa535e889a01e449c772ad088272d1d2ca881
tkiruthika <tkiruthika86@gmail.com>
2022-04-03 12:55:25 -0500

    Adding Files to Earthquakes_past7days

--- Earthquakes_past7days/static/js/logicStep3.js
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -40,9 +40,9 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     return {
       opacity: 1,
       fillOpacity: 1,
-      fillColor: "#ffae42",
+      fillColor: getColor(feature.properties.mag),
       color: "#000000",
-      radius: getRadius(),
+      radius: getRadius(feature.properties.mag),
       stroke: true,
       weight: 0.5
     };
@@ -55,6 +55,25 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     }
     return magnitude * 4;
   }
+  // This function determines the color of the circle based on the magnitude of the earthquake.
+  function getColor(magnitude) {
+   if (magnitude > 5) {
+    return "#ea2c2c";
+    }
+   if (magnitude > 4) {
+     return "#ea822c";
+   }
+   if (magnitude > 3) {
+     return "#ee9c00";
+   }
+   if (magnitude > 2) {
+     return "#eecc00";
+   }
+   if (magnitude > 1) {
+     return "#d4ee00";
+   }
+   return "#98ee00";
+ }
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data, {
 
@@ -64,8 +83,12 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
                 return L.circleMarker(latlng);
             },
         // We set the style for each circleMarker using our styleInfo function.
-        style: styleInfo
-        }).addTo(map);
-  
-
+        style: styleInfo,
+        // We create a popup for each circleMarker to display the magnitude and
+        //  location of the earthquake after the marker has been created and styled.
+        onEachFeature: function(feature, layer) {
+        layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+      }
+    }).addTo(map);
+ 
 });
--- Earthquakes_past7days/static/js/logicStep4.js
+++ Earthquakes_past7days/static/js/logicStep4.js
@@ -1,6 +1,8 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
+
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -15,21 +17,40 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [streets]
+});
+
 // Create a base layer that holds both maps.
 let baseMaps = {
     "Streets": streets,
     "Satellite": satelliteStreets
 };
+// Create the earthquake layer for our map.
+let earthquakes = new L.layerGroup();
+
+// We define an object that contains the overlays.
+// This overlay will be visible all the time.
+let overlays = {
+  Earthquakes: earthquakes
+};
+
+// Then we add a control to the map that will allow the user to change
+// which layers are visible.
+L.control.layers(baseMaps, overlays).addTo(map);
 
 // Create the map object with center, zoom level and default layer.
-let map = L.map('mapid', {
-    center: [39.5, -98.5],
-    zoom: 3,
-    layers: [streets]
-})
+// let map = L.map('mapid', {
+//     center: [39.5, -98.5],
+//     zoom: 3,
+//     layers: [streets]
+// })
 
-// Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
+// // Pass our map layers into our layers control and add the layers control to the map.
+// L.control.layers(baseMaps).addTo(map);
 
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
@@ -40,9 +61,9 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     return {
       opacity: 1,
       fillOpacity: 1,
-      fillColor: "#ffae42",
+      fillColor: getColor(feature.properties.mag),
       color: "#000000",
-      radius: getRadius(),
+      radius: getRadius(feature.properties.mag),
       stroke: true,
       weight: 0.5
     };
@@ -55,6 +76,25 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     }
     return magnitude * 4;
   }
+  // This function determines the color of the circle based on the magnitude of the earthquake.
+  function getColor(magnitude) {
+   if (magnitude > 5) {
+    return "#ea2c2c";
+    }
+   if (magnitude > 4) {
+     return "#ea822c";
+   }
+   if (magnitude > 3) {
+     return "#ee9c00";
+   }
+   if (magnitude > 2) {
+     return "#eecc00";
+   }
+   if (magnitude > 1) {
+     return "#d4ee00";
+   }
+   return "#98ee00";
+ }
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data, {
 
@@ -64,8 +104,14 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
                 return L.circleMarker(latlng);
             },
         // We set the style for each circleMarker using our styleInfo function.
-        style: styleInfo
-        }).addTo(map);
-  
-
+        style: styleInfo,
+        // We create a popup for each circleMarker to display the magnitude and
+        //  location of the earthquake after the marker has been created and styled.
+        onEachFeature: function(feature, layer) {
+        layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+      }
+    }).addTo(earthquakes);
+    // Then we add the earthquake layer to our map.
+    earthquakes.addTo(map);
+ 
 });
